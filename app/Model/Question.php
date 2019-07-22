279ad948223f6e0b8987bc9a61ce8df2a7095440
samiTOUARIA <s.touaria@esi-sba.dz>
2019-07-22 16:46:44 +0100

    Relationships

--- app/Model/Question.php
+++ app/Model/Question.php
@@ -7,4 +7,15 @@ use Illuminate\Database\Eloquent\Model;
 class Question extends Model
 {
     //
+    public function user(){
+         return $this->belongsTo('App\User');
+    }
+
+    public function replies(){
+         return $this->hasMany('App\Mpdel\Reply');
+    }
+
+    public function category(){
+        return $this->belongsTo('App\Model\Category');
+    }
 }
--- app/Model/Reply.php
+++ app/Model/Reply.php
@@ -7,4 +7,15 @@ use Illuminate\Database\Eloquent\Model;
 class Reply extends Model
 {
     //
+    public function question(){
+        return $this->belongsTo('App\Model\Question');
+    }
+
+    public function user(){
+        return $this->belongsTo('App\User');
+    }
+
+    public function likes(){
+        return $this->hasMany('App\Model\Like');
+    }
 }
