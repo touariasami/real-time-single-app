dca7f8499264162c4d76539386fa4a3b690360a8
samiTOUARIA <s.touaria@esi-sba.dz>
2019-07-26 15:14:34 +0100

    Like & Dislike System

--- routes/api.php
+++ routes/api.php
@@ -5,4 +5,7 @@ Route::apiResource('/questions', 'QuestionController');
 Route::apiResource('/categories', 'CategoryController');
 Route::apiResource('/question/{question}/replies', 'ReplyController');
 
+Route::post('/like/{reply}', 'LikeController@likeIt');
+Route::delete('/like/{reply}', 'LikeController@unLikeIt');
+
 
