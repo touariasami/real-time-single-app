81b7b14e6b9873ae06bdd1635ae150a35e56178d
samiTOUARIA <s.touaria@esi-sba.dz>
2019-07-28 11:07:44 +0100

    JWT Package

--- routes/api.php
+++ routes/api.php
@@ -9,3 +9,16 @@ Route::post('/like/{reply}', 'LikeController@likeIt');
 Route::delete('/like/{reply}', 'LikeController@unLikeIt');
 
 
+Route::group([
+
+    'middleware' => 'api',
+    'prefix' => 'auth'
+
+], function () {
+
+    Route::post('login', 'AuthController@login');
+    Route::post('logout', 'AuthController@logout');
+    Route::post('refresh', 'AuthController@refresh');
+    Route::post('me', 'AuthController@me');
+
+});
